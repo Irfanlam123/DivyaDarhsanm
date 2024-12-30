ffc2c3e49cc2e60d9d8c007e81f95609fbb11a70
Irfan Alam <108110097+Irfanlam123@users.noreply.github.com>
2024-12-30 17:05:23 +0530

    changes in files

--- src/Components/ImageCarousel/ImageCarousel.jsx
+++ src/Components/ImageCarousel/ImageCarousel.jsx
@@ -2,25 +2,36 @@ import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-import Temp1 from "../../../src/assets/temp1.jpg";
+// import Temp1 from "../../../src/assets/temp1.jpg";
 import Temp2 from "../../../src/assets/temp2.jpg";
-import Temp3 from "../../../src/assets/temp3.jpg";
-
+// import Temp3 from "../../../src/assets/temp3.jpg";
+import Temp4 from "../../../src/assets/Temp4.jpg";
+import Temp5 from "../../../src/assets/temp5.jpg";
 const ImageCarousel = () => {
   const images = [
-    {
-      id: 1,
-      src: Temp1,
-      alt: "Slide 1",
-    },
+    // {
+    //   id: 1,
+    //   src: Temp1,
+    //   alt: "Slide 1",
+    // },
     {
       id: 2,
       src: Temp2,
       alt: "Slide 2",
     },
+    // {
+    //   id: 3,
+    //   src: Temp3,
+    //   alt: "Slide 3",
+    // },
     {
-      id: 3,
-      src: Temp3,
+      id: 4,
+      src: Temp4,
+      alt: "Slide 3",
+    },
+    {
+      id: 5,
+      src: Temp5,
       alt: "Slide 3",
     },
   ];
